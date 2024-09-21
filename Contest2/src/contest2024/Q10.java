f032c8efa78eff5a6c4003e251598734e34905df
mengl <mengl@Menglixing>
2024-09-21 17:28:50 +0800

    d

--- Contest2/src/contest2024/Q10.java
+++ Contest2/src/contest2024/Q10.java
@@ -9,7 +9,7 @@ public class Q10 {
 		// TODO Auto-generated method stub
 		int N=15;
 		int arr[]= {11,9,6,13,8,14,5,12,7,10,15,5,13,6,10};
-		int maximum=4;
+		int maximum=4; 
 		
 		int[] arrCooked=new int[maximum];
 		
